5e5c2ef94ccff673e1b9b194bcace139347d92b9
Amine <Amine@172.16.187.195>
2013-11-18 11:39:06 +0100

    Premier commit dans le repository

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Amine
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ceci est un test d'application avec contrôle de vesion");
+        // TODO code application logic here
+    }
+}
